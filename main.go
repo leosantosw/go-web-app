9938b6ab01fadaafc049398d4cab7b0b3bec5d63
leosantosw <leonardosantosx0077@gmail.com>
2025-02-16 18:49:17 -0300

    render product struct

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import (
 	"text/template"
 )
 
+type Product struct {
+	Name        string
+	Description string
+	Price       float64
+	Quantity    int
+}
+
 var templates = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
@@ -13,5 +20,25 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "Index", nil)
+	products := []Product{
+		{
+			Name:        "Camisa GO",
+			Description: "Camisa estilosa e confortável, perfeita para desenvolvedores.",
+			Quantity:    10,
+			Price:       100.00,
+		},
+		{
+			Name:        "Caneca GO",
+			Description: "Caneca de cerâmica com o logo da linguagem Go.",
+			Quantity:    20,
+			Price:       25.00,
+		},
+		{
+			Name:        "Livro 'A Linguagem de Programação Go'",
+			Description: "Livro oficial para aprender Go.",
+			Quantity:    30,
+			Price:       80.00,
+		},
+	}
+	templates.ExecuteTemplate(w, "Index", products)
 }
