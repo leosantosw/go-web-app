25f5809ae495fabb775eeee73c012fe892e5683e
leosantosw <leonardosantosx0077@gmail.com>
2025-02-16 21:37:23 -0300

    feat: connect to postgres database

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"net/http"
 	"text/template"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "leosantosw"
+	password = "leosantosw"
+	dbname   = "postgres"
 )
 
 type Product struct {
@@ -19,7 +31,23 @@ func main() {
 	http.ListenAndServe(":3000", nil)
 }
 
+func databaseConn() *sql.DB {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disabled",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
+
 func index(w http.ResponseWriter, r *http.Request) {
+	db := databaseConn()
+	db.Close()
+
 	products := []Product{
 		{
 			Name:        "Camisa GO",
